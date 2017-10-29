35814e990079ee50a064d74cebcb5b2b55ef7a95
Jorge <Jcvarela@live.com>
2017-10-28 20:09:13 -0400

    Workign on presenting all controllers

--- modules/my_controllers/client/controllers/my_controllers.client.controller.js
+++ modules/my_controllers/client/controllers/my_controllers.client.controller.js
@@ -3,6 +3,22 @@
 // Create the 'chat' controller
 angular.module('my_controllers').controller('MyControllerController', ['$scope',
   function ($scope) {
+    $scope.controllers = [{
+      title: "My TV"
+    }, {
+      title: "My 2nd TV"
+    }, {
+      title: "fasdjka fkjsdafkj a"
+    }, {
+      title: "fldslfldlfldlfldlf"
+    }, {
+      title: "fdaslkfks dkfsdk fksdkf kdfk sd"
+    }, {
+      title: "My 2nd TV"
+    }];
 
+    $scope.openModal = function () {
+      console.log("helo world");
+    };
   }
 ]);
